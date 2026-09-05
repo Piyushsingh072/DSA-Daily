6a40798d82650a46ad56188fdae87f71db34fbc2
Piyush Singh <piyushsingh54823@gmail.com>
2026-09-05 23:11:05 +0530

    5th Question

---
+++ Sum_of_Elements.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {5,10,15,20,25};
+    int Sum = 0;
+for (int i = 0; i<5; i++) {
+    Sum += arr[i];
+}
+cout<<Sum;
+return 0;
+}
+
